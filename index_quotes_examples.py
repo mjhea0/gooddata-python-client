6538a146417e8964b46fc92537eaf748d2ac4abf
mjhea0 <hermanmu@gmail.com>
2012-09-24 19:37:13 -0700

    index.py update

--- index_quotes_examples.py
+++ index_quotes_examples.py
@@ -2,7 +2,7 @@ from gooddataclient.connection import Connection
 from gooddataclient.project import Project
 from datasets.quotes import Quotes
 
-connection = Connection('miketest802@gmail.com', 'testgood')
+connection = Connection('email', 'password')
 proj_name = 'quotesFinal9242012'
 print '\n\n\nLoading Project: ', proj_name
 project = Project(connection).create(name=proj_name)
